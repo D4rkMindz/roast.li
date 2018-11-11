1430a5c4216992be52e5aa13e177ac3f0ee209b2
D4rkMindz <bjoern.pfoster@gmail.com>
2018-11-12 00:19:16 +0100

    Updated env example

--- config/env.example.php
+++ config/env.example.php
@@ -1,10 +1,14 @@
 <?php
-$config['displayErrorDetails'] = true;
+$env['displayErrorDetails'] = true;
 
-$env['db_test']['host'] = '';
-$env['db_test']['port'] = '';
-$env['db_test']['username'] = '';
-$env['db_test']['password'] = '';
+$env['db']['database'] = 'mydb';
+$env['db']['charset'] = 'utf8';
+$env['db']['encoding'] = 'utf8';
+$env['db']['collation'] = 'utf8_unicode_ci';
+$env['db']['host'] = '';
+$env['db']['port'] = '';
+$env['db']['username'] = '';
+$env['db']['password'] = '';
 
 $env['db_test']['host'] = '127.0.0.1';
 $env['db_test']['port'] = '3306';
